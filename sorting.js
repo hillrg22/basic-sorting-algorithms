a4f9f7df761c4ba60334be52d1fb1be9273ea1a2
hillrg22 <hillrg22@gmail.com>
2018-12-18 13:28:39 -0700

    added swap function for sorting algorithms

--- sorting.js
+++ sorting.js
@@ -1,7 +1,10 @@
 // Write a swapping function
 
 function swap(array, index1, index2) {
-
+    let temp = array[index1]
+    array[index1] = array[index2]
+    array[index2] = temp
+  return array
 }
 
 // Implement Bubble Sort
