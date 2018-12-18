65140d27efd44e738fb53369e67c92fafde1c114
hillrg22 <hillrg22@gmail.com>
2018-12-18 14:29:59 -0700

    added bubbleSort and selectionSort functions

--- sorting.js
+++ sorting.js
@@ -10,13 +10,44 @@ function swap(array, index1, index2) {
 // Implement Bubble Sort
 
 function bubbleSort(array) {
+  var done = false;
 
+  while(!done) {
+    done = true;
+    for (let i=1; i< array.length; i++){
+      if (array[i-1] > array[i]){
+        done = false;
+        swap(array, (i-1), i)
+      }
+    }
+  }
+  return array
 }
 
 // Implement Selection Sort
 
 function selectionSort(array) {
+let currentMinimum = array[0];
+let minIndex = 0;
+let n = 0;
 
+  while(n < array.length-1){
+    // console.log("n =", n, "array.length -1: ", (array.length-1))
+    for (let i = n; i < array.length; i++){
+      if(array[i]< currentMinimum){
+        currentMinimum = array[i]
+        minIndex = i
+      }
+    }
+    swap(array, n, minIndex)
+    n++
+    currentMinimum = array[n]
+    minIndex = n
+
+
+  }
+
+  return array
 }
 
 // Sample data
@@ -24,4 +55,6 @@ const arraySample = [5, 8, 13, 1, 9, 43, 19, 33]
 
 
 bubbleSort(arraySample)
-selectionSort(arraySample)
+console.log(selectionSort(arraySample))
+
+// console.log(swap(arraySample, 1,5))
